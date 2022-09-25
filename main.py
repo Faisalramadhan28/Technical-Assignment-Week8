7f9e9f6aae665ecead3fe406415a6cf07e4f162f
Faisalramadhan28 <107247968+Faisalramadhan28@users.noreply.github.com>
2022-09-25 18:29:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import pymongo
 from flask import Flask,request
 
 
-client = pymongo.MongoClient("mongodb+srv://ramadhanisal:ramadhanisal@cluster0.vk2qhxu.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://sunrise:<ramadhanisal>@cluster0.honyafl.mongodb.net/?retryWrites=true&w=majority")
 
-db = client.faisal
-collection = db.sunrise
+db = client.sunrise
+collection =db.smkn4
 
 def sans (kecepatan,latitude,longitude) -> tuple:
     try:
